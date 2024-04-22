5cba681f2abf567aedfa30e04cb7926fb9211e0d
Nicolas Vaagen <37600758+nicvagn@users.noreply.github.com>
2024-04-22 17:14:16 -0600

    forked own repo

---
+++ custom/aliases/nrv.aliases.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# git aliases for convinince
+alias push="git push"
+alias fetch="git fetch"
+alias pull="git pull"
+alias commit="git commit"
+alias status="git status"
+alias checkout="git checkout"
+alias add="git add"
+alias gdiff="git diff"
+
+# conviniantly enter nl pyenv
+alias nl_env=". ~/dev/NicLink/pyenv_up.sh; cd ~/dev/NicLink/"
+
+# cd aliases
+alias ...="cd ../.."
+alias ....="cd ../../.."
